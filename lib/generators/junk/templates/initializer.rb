736622b59e744f5b2ca033f30d10f11252a1ca49
Eric Laquer <Eric.Laquer@tangentenergy.com>
2025-08-11 10:27:00 -0400

    removed junk

--- lib/generators/junk/templates/initializer.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-ActiveViewComponent.configure do |config|
-  # Enable/disable the ActiveViewComponent system
-  config.enabled = true
-  
-  # Automatically persist component trees during rendering
-  config.auto_persist = true
-end
